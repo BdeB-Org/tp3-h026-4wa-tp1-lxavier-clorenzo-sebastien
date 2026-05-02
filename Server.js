85ee279c86864d9fc3127c8eeac1d4a0ad59f4fb
Sebastien Saint-Cyr <2466143@bdeb.qc.ca>
2026-05-02 09:32:30 -0400

    Ajout du code d'exemple pour les nouveaux éléments

--- Server.js
+++ Server.js
@@ -1,13 +1,25 @@
-const express = require("express");
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-const ProjetsEtudiantsRoutes = require("./Routes/ProjetsEtudiantsRoutes");
+// Initialise la BD
+require('./Config/ProjetsEtudiants.js');
 
 app.use(express.json());
+app.use(express.static('public'));
 
-app.use("/", ProjetsEtudiantsRoutes);
+const etudiantRoutes = require('./Routes/ProjetsEtudiantsRoutes');
+const authRoutes = require('./Routes/authRoutes');
 
-app.listen(3000, () => {
-    console.log("Serveur lancé sur http://localhost:3000");
+app.use('/api/etudiants', ProjetsEtudiantsRoutes);
+app.use('/api/auth', authRoutes);
+
+// Redirection par défaut
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
